f864c2ad97fc7861ae68bf15089bee9d073d1ea4
Suyog Ghimire <ghimiresuyog89@gmail.com>
2025-05-22 18:55:34 +0545

    added argpraser

--- src/inference.py
+++ src/inference.py
@@ -6,25 +6,34 @@ import numpy as np
 from PIL import Image
 from model import Unet
 from torch import utils
+import argparse
+#----------------Define and Parse CLI arguments--------------------
+parser = argparse.ArgumentParser(description = "Inference with trained model")
+parser.add_argument("--model_path",type=str,required=True, help='Path to model')
+parser.add_argument("--image_path",type = str, required = True, help = 'Path to the test image')
 
+args = parser.parse_args()
+
+#---------------- Set Device -------------------------------------
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
+#---------------- Define Transforms-------------------------------
 transform = A.Compose([
     A.Resize(512, 512),
     A.Normalize(mean=(0.5,), std=(0.5,), max_pixel_value=255.0),
     ToTensorV2()
 ])
 
-## loading the saved model
+#----------------Load Models-----------------------------------------
 model = Unet(in_channels=3,out_channels= 1)
 
-model.load_state_dict(torch.load("C:/Users/Acer nitro/Desktop/retinal-vessel-segmentation/models/best_unet.pth", map_location=device))
+model.load_state_dict(torch.load(args.model_path, map_location=device))
 model.to(device)
 model.eval()
 
-#loading and preprocessing the image
+#---------------Loading and preprocessing the image-------------------------
 
-img_path = "C:/Users/Acer nitro/Desktop/retinal-vessel-segmentation/data/DRIVE/test/images/01_test.tif"
+img_path = args.image_path
 
 image = Image.open(img_path).convert("RGB")
 image_np = np.array(image)
@@ -35,12 +44,12 @@ input_tensor = transformed['image'].unsqueeze(dim = 0).to(device)
 
 print("Model loaded and ready.")
 print("Starting inference...")
-# inference
+#--------------------Inference--------------------------------
 with torch.no_grad():
     pred = torch.sigmoid(model(input_tensor))
     pred_mask = (pred > 0.5).float().squeeze().cpu().numpy()
 
-
+#--------------------Plot the results-------------------------
 plt.figure(figsize=(10,5))
 plt.subplot(1,2,1)
 
